a8a7d112a80a68f48ae3cd55d9e2b8ac2c3adcb7
Nadir N. Mammadaliyev <nadir.mammadaliyev@kapitalbank.az>
2022-10-18 12:37:54 +0400

    first commit

---
+++ src/main/java/az/nadir/newtestapp/NewTestAppApplication.java
@@ -0,0 +1,13 @@
+package az.nadir.newtestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewTestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/nadir/newtestapp/NewTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package az.nadir.newtestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
